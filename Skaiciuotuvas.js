dac4266653d1432b48953b17a6d5bdeefc39b4da
Laurynas Leišis <lauris.leisis@gmail.com>
2019-10-20 18:58:06 +0300

    Fixed and added some functions

--- Skaiciuotuvas.js
+++ Skaiciuotuvas.js
@@ -10,6 +10,11 @@ var operatorArray = new Array();
 
 function number(num) {
 	input = document.getElementById('input');
+	console.log(input.value.charAt(input.value.length-1));
+	if(input.value.charAt(input.value.length-1) == '=') {
+		input.value = "";
+		tempValue = "";
+	}
 	switch(num){
 		case 1:
 			input.value += '1';
@@ -66,6 +71,7 @@ function number(num) {
 }
 function operand(op){
 	input = document.getElementById('input');
+	if(input.value.charAt(input.value.length-1) != '='){
 	if(firstOperand.length > 0 || secondOperand.length > 0){
 	numberArray.push(firstOperand);
 	firstOperand = "";
@@ -87,20 +93,26 @@ function operand(op){
 			input.value += '/';
 			operator = '/';
 			break;
+		case '^':
+			input.value += '^';
+			operator = '^';
 		}
 	}
 	operatorArray.push(operator);
-
+}
 }
 function roots(op) {
 	input = document.getElementById('input');
 	var open = true;
-			
+	if(input.value.charAt(input.value.length-1) != '='){		
 	switch(op){
 		case '√':
 			input.value += '√(';
 
 			operator = '√';
+			break;
+		case '^':
+			input.value += '^';
 			break;
 		case ')':
 			input.value += ')';
@@ -110,6 +122,7 @@ function roots(op) {
 			input.value += '(';
 			break;
 		}
+	}
 		if(open == false ){
 				tempValue = "";
 				var searching = new String();
@@ -117,8 +130,10 @@ function roots(op) {
 			var stringVar = searching[1];
 			//searching = searching.replace(/\(|\)/, "");
 			tempValue = input.value.replace(/\(([^)]+)\)/, calculate(stringVar));
+			console.log(tempValue);
 		}
-		
+
+
 		//operatorArray.push(op);
 }
 
@@ -141,90 +156,131 @@ function calculate(string){
 	
 	var len 
 	len = oper.length;
-	for(var j = 0; j < len; j++){
-			if(j == 0 ) rez = firstop[0];
-			console.log(rez);
-		switch(oper[j]){
-			case '+':
-				rez = parseInt(rez, 10) + parseInt(firstop[j+1], 10);
-				break;
-			case '-':
-				rez = parseInt(rez, 10) - parseInt(firstop[j+1], 10);
-				break;
-			case '*':
-				rez = parseInt(rez, 10) * parseInt(firstop[j+1], 10);
-				break;
-			case '/':
-				rez = parseInt(rez, 10) / parseInt(firstop[j+1], 10);
-				break;
+
+	if(oper.length <1) rez = string;
+	else{
+		for(var j = 0; j < len; j++){
+				if(j == 0 ) rez = firstop[0];
+				console.log(rez);
+			switch(oper[j]){
+				case '+':
+					rez =parseFloat(rez) +parseFloat(firstop[j+1]);
+					break;
+				case '-':
+					rez =parseFloat(rez) -parseFloat(firstop[j+1]);
+					break;
+				case '*':
+					rez =parseFloat(rez) *parseFloat(firstop[j+1]);
+					break;
+				case '/':
+					rez =parseFloat(rez) /parseFloat(firstop[j+1]);
+					break;
+
+			}
 
 		}
-
 	}
 	console.log(rez);
 	return rez;
 }
+function convert(string){
+	switch(string.charAt(0)){
+		case '√':
+			string = Math.pow(parseFloat(string.substr(1, string.length-1), 10), 1/2);
+	}
+	for(var i = 0; i < string.length;i++){
+		if(isNaN(string[i])){
+			switch(string[i]){
+				case '^':
+					string = Math.pow(parseFloat(string.substr(0, i)), parseFloat(string.substr(i+1, string.length-1)));
+					break;
+			}
+		}
+	}
+
+
+	return string;
+}
+function regexSearch(tempValue){
+	var searchRoot = new String();
+	var searcRootnth = new String();
+	var searchsquared = new String();
+	searchRoot = tempValue.match(/√[0-9]+/);
+	console.log(searchRoot);
+	if(searchRoot != null ) tempValue = tempValue.replace(/√[0-9]+/, convert(searchRoot[0]));
+
+	searchsquared = tempValue.match(/[0-9]+\^[0-9]+/);
+	console.log(tempValue);
+	if(searchsquared != null ) tempValue = tempValue.replace(/[0-9]+\^[0-9]+/, convert(searchsquared[0]));
+	return tempValue;
+}
 function resultop() {
+	if(input.value.charAt(input.value.length-1) != '='){
 	input.value += '=';
+	tempValue += '';
 	result = "";
 	var firstop = new Array();
 	var oper = new Array();
 	var temp = 0;
 	var tempAt = 0;
+
 	if(tempValue.length <1) tempValue = input.value;
 
-	for(var i = 0; i < tempValue.length; i++){
-		if(isNaN(tempValue.charAt(i))) {
-			firstop.push(tempValue.substr(tempAt, i));
-			oper.push(tempValue.charAt(i));
-			tempAt =i+1;				
-			}
+	tempValue = regexSearch(tempValue);
 
+	console.log(tempValue);
+
+
+	if() 
+
+	for(var i = 0; i < tempValue.length; i++){
+		if(isNaN(tempValue.charAt(i))){
+			//console.log(tempValue.charAt(i));
+		//	if(tempValue.charAt(i) == '+' || tempValue.charAt(i) == '-' || tempValue.charAt(i) == '*' ||  tempValue.charAt(i) == '/') {
+				oper.push(tempValue.charAt(i));
+				firstop.push(tempValue.substr(tempAt, i));
+				console.log(tempValue.charAt(i));
+				tempAt =i+1;			
+			//}
+
+			
 		}
-	
+	}
 	var len 
 	len = oper.length;
-	for(var j = 0; j < len; j++){
-			if(j == 0 ) result = firstop[0];
-			console.log(firstop[j+1]);
-		switch(oper[j]){
-			case '+':
-				result = parseInt(result, 10) + parseInt(firstop[j+1], 10);
-				break;
-			case '-':
-				result = parseInt(result, 10) - parseInt(firstop[j+1], 10);
-				break;
-			case '*':
-				result = parseInt(result, 10) * parseInt(firstop[j+1], 10);
-				break;
-			case '/':
-				result = parseInt(result, 10) / parseInt(firstop[j+1], 10);
-				break;
-			case '√':
-				if(oper[j-1] == undefined) result = Math.pow(parseInt(firstop[j], 10), 1/2);
-				else{
-					switch(oper[j-1]){
+	if(oper.length < 1) {
+		result = parseFloat(tempValue);
+	}
+	else{
+				for(var j = 0; j < len; j++){
+						if(j == 0 ) result = firstop[0];
+						console.log(firstop[j+1]);
+					switch(oper[j]){
 						case '+':
-							result = parseInt(result, 10) + Math.pow(parseInt(firstop[j+1], 10), 1/2);
+							result =parseFloat(result) +parseFloat(firstop[j+1]);
 							break;
 						case '-':
-							result = parseInt(result, 10) - Math.pow(parseInt(firstop[j+1], 10), 1/2);
+							result =parseFloat(result) -parseFloat(firstop[j+1]);
 							break;
 						case '*':
-							result = parseInt(result, 10) * Math.pow(parseInt(firstop[j+1], 10), 1/2);
+							result =parseFloat(result) *parseFloat(firstop[j+1]);
 							break;
 						case '/':
-							result = parseInt(result, 10) / Math.pow(parseInt(firstop[j+1], 10), 1/2);
+							result =parseFloat(result) /parseFloat(firstop[j+1]);
+							break;
+						case '^':
+							result = Math.pow(parseFloat(result),parseFloat(firstop[j+1])); 
 							break;
 					}
-				}
-				break;
-		}
 
-	}
+				}
+			}
+	
 	var output = document.getElementById('output');
 	output.value = result;
+	console.log(result);
 	result = "";
+}
 }
 function clearText(){
 	 input = document.getElementById('input');
@@ -237,6 +293,7 @@ function clearText(){
     secondOperand = "";
     operator = "";
     result = "";
+    tempValue = "";
 }
 function deleteInput(){
 	var input = document.getElementById('input');
